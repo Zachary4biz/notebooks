6c3f3a93671b1dc55e1885c76c0f516dc7d7db14
zach <zachary4biz@yeah.net>
2018-12-08 12:05:34 +0800

    default

--- git_update.sh
+++ git_update.sh
@@ -1,6 +1,11 @@
 
 # also add an alias in ~/.bashrc
 # ==> alias gup="sh git_update.sh" 
+####################################
+# use ssh to access the repository.
+# git remote rm origin
+# git remote add origin git@github.com:Zachary4biz/notebooks.git
+# 
 git add .
 git commit -am "default"
 git push
