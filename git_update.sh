b0f26cc4175caf81fb40b10972ef7d76cfe68b31
zach <zachary4biz@yeah.net>
2018-12-08 12:15:21 +0800

    default

--- git_update.sh
+++ git_update.sh
@@ -5,8 +5,15 @@
 # use ssh to access the repository.
 # git remote rm origin
 # git remote add origin git@github.com:Zachary4biz/notebooks.git
-# 
+#
+
+commit_info=$1
+if [ ! -n "$commit_info" ]; then
+    echo "use default commit_info"
+    commit_info="default"
+fi
+ 
 git add .
-git commit -am "default"
+git commit -am "$commit_info"
 git push
 
