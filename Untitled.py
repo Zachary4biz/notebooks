73712998f03862e5eae7bd7e833eb5d1bf7af91c
zhoutong <zachary4biz@yeah.net>
2018-12-08 11:47:01 +0800

    default

---
+++ Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sys
+print(sys.version)
+
+
+# ### a title
+
+# In[3]:
+
+
+print("abcdefg hh")
+
+
+# In[4]:
+
+
+print("ppiiweptd")
+
